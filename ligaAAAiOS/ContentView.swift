2f3da8dd60e8cb6e7f9fd641418f5ac0963c9150
Ignacio Hernaiz <nacho_celtic@hotmail.com>
2023-12-21 10:59:23 +0100

    Initial Commit

---
+++ ligaAAAiOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ligaAAAiOS
+//
+//  Created by IGNACIO HERNAIZ IZQUIERDO on 21/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ligaAAAiOS/ligaAAAiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ligaAAAiOSApp.swift
+//  ligaAAAiOS
+//
+//  Created by IGNACIO HERNAIZ IZQUIERDO on 21/12/23.
+//
+
+import SwiftUI
+
+@main
+struct ligaAAAiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
