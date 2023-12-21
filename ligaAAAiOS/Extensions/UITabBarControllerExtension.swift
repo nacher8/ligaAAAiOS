ba23d2c70cdc3c0b61d7fb219610fe8b9645199d
Ignacio Hernaiz <nacho_celtic@hotmail.com>
2023-12-21 13:03:14 +0100

    Added tab bar and localizable files.

---
+++ ligaAAAiOS/Extensions/UITabBarControllerExtension.swift
@@ -0,0 +1,39 @@
+//
+//  UITabBarControllerExtension.swift
+//  ligaAAAiOS
+//
+//  Created by IGNACIO HERNAIZ IZQUIERDO on 21/12/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension UITabBarController {
+    open override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+
+        tabBar.layer.masksToBounds = true
+        tabBar.layer.cornerRadius = 8
+        // Choose with corners should be rounded
+        tabBar.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner] // top left, top right
+
+        // Uses `accessibilityIdentifier` in order to retrieve shadow view if already added
+        if let shadowView = view.subviews.first(where: { $0.accessibilityIdentifier == "TabBarShadow" }) {
+            shadowView.frame = tabBar.frame
+        } else {
+            let shadowView = UIView(frame: .zero)
+            shadowView.frame = tabBar.frame
+            shadowView.accessibilityIdentifier = "TabBarShadow"
+            shadowView.backgroundColor = UIColor.white
+            shadowView.layer.cornerRadius = tabBar.layer.cornerRadius
+            shadowView.layer.maskedCorners = tabBar.layer.maskedCorners
+            shadowView.layer.masksToBounds = false
+            shadowView.layer.shadowColor = Color.black.cgColor
+            shadowView.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+            shadowView.layer.shadowOpacity = 0.3
+            shadowView.layer.shadowRadius = 10
+            view.addSubview(shadowView)
+            view.bringSubviewToFront(tabBar)
+        }
+    }
+}
